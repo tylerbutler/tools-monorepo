e5c1efc2f57a66fdaf104355dcb91f2d9291627b
Tyler Butler <tyler@tylerbutler.com>
2024-08-04 17:10:29 -0700

    updates

--- packages/fundamentals/README.md
+++ packages/fundamentals/README.md
@@ -3,4 +3,4 @@
 This package contains fundamental, zero-dependency functions and classes that I often need in my projects. 
 
 See [the API summary](https://github.com/tylerbutler/tools-monorepo/blob/main/packages/fundamentals/docs/README.md) for
-an overview of the API. Public APIs should all be documented, but the docs aren't yet published anywhere.
+an overview of the API.
--- packages/fundamentals/typedoc.config.cjs
+++ packages/fundamentals/typedoc.config.cjs
@@ -9,10 +9,12 @@ module.exports = {
 		"./src/set.ts",
 	],
 	plugin: ["typedoc-plugin-markdown"],
+	gitRevision: "main",
 	sourceLinkTemplate:
-		"https://github.com/tylerbutler/tools-monorepo/blob/main/{path}#L{line}",
+		"https://github.com/tylerbutler/tools-monorepo/blob/{gitRevision}/{path}#L{line}",
 	outputFileStrategy: "modules",
 	out: "docs",
 	// entryModule: "index",
+	readme: "./README.md",
 	mergeReadme: true,
 };
