2c30226f909bc331746749c5b44ff4e2c03749d4
Tyler Butler <tyler@tylerbutler.com>
2024-08-17 15:31:03 -0700

    docs(dill): Update dill docs (#62)

---
+++ packages/dill/test/common.ts
@@ -0,0 +1,7 @@
+export const testUrls = [
+	"http://localhost:8080/test0.json",
+	"http://localhost:8080/test1.json",
+	"http://localhost:8080/tarball2.tar.gz",
+	"http://localhost:8080/tarball3.tar",
+	"http://localhost:8080/test4.zip",
+] as const;
