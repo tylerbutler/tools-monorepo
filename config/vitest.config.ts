dab83e7c91c276d2a97c0820de7c4b5fa7fd2561
Tyler Butler <tyler@tylerbutler.com>
2024-08-18 18:17:48 -0700

    config

--- config/vitest.config.ts
+++ config/vitest.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "vitest/config";
 const config = defineConfig({
 	test: {
 		reporters: process.env.GITHUB_ACTIONS
-			? ["github-actions", "junit"]
-			: ["verbose", "junit"],
+			? // CI mode
+				["github-actions", "junit"]
+			: // local mode
+				["verbose", "junit"],
 		outputFile: {
 			junit: "./_temp/junit.xml",
 		},
