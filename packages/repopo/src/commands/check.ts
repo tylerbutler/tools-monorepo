f92b5461603c6949fe6e6c344d8d4584f8fc774b
Tyler Butler <tyler@tylerbutler.com>
2024-10-04 19:15:05 -0700

    updates

--- packages/repopo/src/commands/check.ts
+++ packages/repopo/src/commands/check.ts
@@ -114,38 +114,16 @@ export class CheckPolicy extends GitCommand<
 	// private policies: RepoPolicy[] | undefined;
 	private commandContext: CheckPolicyCommandContext | undefined;
 
-	public override get defaultConfig() {
+	public get defaultConfig() {
 		return DefaultPolicyConfig;
-	}
-
-	protected override async loadConfig(): Promise<PolicyConfig> {
-		const gitRoot = await findGitRoot();
-		// this.configPath ??= this.config.configDir; // path.join(this.config.configDir, "config.ts");
-		const explorer = cosmiconfig(this.config.bin, {
-			searchStrategy: "global",
-		});
-		this.verbose(`Looking for '${this.config.bin}' config at '${gitRoot}'`);
-		const config: CosmiconfigResult = await explorer.search(gitRoot);
-		if (config?.config !== undefined) {
-			this.verbose(`Found config at ${config.filepath}`);
-		}
-		if (config?.config === undefined) {
-			this.warning("No config found; using defaults.");
-		}
-		const finalConfig: PolicyConfig = config?.config ?? this.defaultConfig;
-		if (finalConfig.includeDefaultPolicies === true) {
-			finalConfig.policies ??= [];
-			finalConfig.policies.push(...DefaultPolicies);
-		}
-
-		return finalConfig;
 	}
 
 	public override async init(): Promise<void> {
 		await super.init();
 
+		const config = await this.loadConfig();
 		let policies =
-			this.commandConfig?.policies?.filter((h) => {
+			config?.policies?.filter((h) => {
 				if (
 					this.flags.excludePolicy === undefined ||
 					this.flags.excludePolicy.length === 0
@@ -177,10 +155,9 @@ export class CheckPolicy extends GitCommand<
 		}
 
 		const excludeFiles: RegExp[] =
-			this.commandConfig?.excludeFiles?.map((e) => toRegExp(e)) ?? [];
+			config?.excludeFiles?.map((e) => toRegExp(e)) ?? [];
 
-		const excludePoliciesForFilesRaw =
-			this.commandConfig?.excludePoliciesForFiles;
+		const excludePoliciesForFilesRaw = config?.excludePoliciesForFiles;
 
 		const excludePoliciesForFiles: ExcludedPolicyFileMap = new Map();
 		if (excludePoliciesForFilesRaw) {
@@ -211,35 +188,14 @@ export class CheckPolicy extends GitCommand<
 		// 	this.log(`${handlersToRun.length} TOTAL POLICY HANDLERS`);
 		// 	this.exit(0);
 		// }
-
-		const policies = this.commandConfig?.policies ?? [];
+		const config = await this.loadConfig();
+		const policies = config?.policies ?? [];
 		this.verbose(`${policies.length} policies loaded.`);
 		for (const h of policies) {
 			this.verbose(h.name);
 		}
 
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-		// const rawExclusions: string[] =
-		// 	this.flags.exclusions === undefined
-		// 		? manifest.policy?.exclusions
-		// 		: await readJson(this.flags.exclusions);
-
-		// const exclusions: RegExp[] = rawExclusions.map((e) => new RegExp(e, "i"));
-
-		// const rawHandlerExclusions = manifest?.policy?.handlerExclusions;
-
-		// const handlerExclusions: ExcludedPolicyFileMap = {};
-		// if (rawHandlerExclusions) {
-		// 	for (const rule of Object.keys(rawHandlerExclusions)) {
-		// 		handlerExclusions[rule] = rawHandlerExclusions[rule].map(
-		// 			(e) => new RegExp(e, "i"),
-		// 		);
-		// 	}
-		// }
-
 		const filePathsToCheck: string[] = [];
-		// const context = await this.getContext();
-		// const gitRoot = context.repo.resolvedRoot;
 
 		if (this.flags.stdin) {
 			const stdInput = await readStdin();
@@ -302,6 +258,7 @@ export class CheckPolicy extends GitCommand<
 		// Use the repo-relative path so that regexes that specify string start (^) will match repo paths.
 		// Replace \ in result with / in case OS is Windows.
 		const relPath = path.relative(gitRoot, file).replace(/\\/g, "/");
+		const config = await this.loadConfig();
 
 		await Promise.all(
 			policies
