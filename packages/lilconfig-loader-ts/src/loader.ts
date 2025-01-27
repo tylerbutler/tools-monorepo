b0120e8d630ba7ee539f4b5bc560e8fbe18a9a57
Tyler Butler <tyler@tylerbutler.com>
2025-01-27 13:32:09 -0800

    builds

--- packages/lilconfig-loader-ts/src/loader.ts
+++ packages/lilconfig-loader-ts/src/loader.ts
@@ -9,12 +9,14 @@ const jiti = createJiti(import.meta.url);
 /**
  * A barebones TypeScript module importer. It imports and returns the default export from the TypeScript file at
  * filepath.
- * 
+ *
  * @param filepath - The path to a TypeScript file to load.
  * @param _contents - The contents of the config file. **This property is unused. TypeScript modules are always loaded
  * from the file system.**
  * @throws If the file cannot be found or imported.
-= */
+ *
+ * @alpha
+ */
 export const TypeScriptLoader: Loader = async (
 	filepath: string,
 	_contents: string,
