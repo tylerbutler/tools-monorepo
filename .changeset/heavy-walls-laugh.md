daa06cff39b90e3514bf96c5ae851d14be8b10ca
Tyler Butler <tyler@tylerbutler.com>
2025-02-12 17:42:18 -0800

    refactor(cli-api): replace cosmicconfig with lilconfig (#157)

---
+++ .changeset/heavy-walls-laugh.md
@@ -0,0 +1,9 @@
+---
+"@tylerbu/cli-api": minor
+---
+
+API changes
+
+- **Breaking change:** `CommandWithConfig.configPath` is now `CommandWithConfig.configLocation`.
+- `PackageTransformer`s can now be async.
+- `readJsonWithIndent` is now generic.
---
+++ packages/cli-api/src/commands/configTest.ts
@@ -0,0 +1,23 @@
+import { CommandWithConfig } from "@tylerbu/cli-api";
+
+export interface TestConfig {
+	stringProperty: string;
+}
+
+/**
+ * An implementation of CommandWithConfig used for testing.
+ */
+export default class ConfigTestCommand extends CommandWithConfig<
+	typeof ConfigTestCommand,
+	TestConfig
+> {
+	protected override defaultConfig: TestConfig | undefined = {
+		stringProperty: "default",
+	};
+
+	// biome-ignore lint/suspicious/useAwait: inherited method
+	override async run(): Promise<TestConfig | undefined> {
+		this.log(`Loaded config from: ${this.configLocation}`);
+		return this.commandConfig;
+	}
+}
