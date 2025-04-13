60e3ef96bd5cc5c73d8bd65655ee44c5923332d5
Tyler Butler <tyler@tylerbutler.com>
2025-04-12 18:12:50 -0700

    wip

--- packages/fluid-svelte/src/lib/example.svelte.ts
+++ packages/fluid-svelte/src/lib/example.svelte.ts
@@ -3,35 +3,30 @@ import { SchemaFactory, Tree } from "fluid-framework";
 export const sf = new SchemaFactory("AppSchema");
 
 const ExampleSchema = sf.object("SchemaKey", {
-	numVal: sf.number,
-	stringVal: sf.string,
+	__num: sf.number,
+	__string: sf.string,
 });
 
-export class TreeData extends ExampleSchema {
-	private _num = $state(this.numVal);
-	public get num() {
-		return this._num;
-	}
-	public set num(v: number) {
-		this.numVal = v;
+export class MyDataClass extends ExampleSchema {
+	#num = $state(this.__num);
+	#string = $state(this.__string);
+
+	// Clean getters/setters that maintain reactivity
+	get num() { return this.#num; }
+	set num(v: number) { 
+		this.__num = v;
 	}
 
-	private _string = $state(this.stringVal);
-	public get string() {
-		return this._string;
-	}
-	public set string(v: string) {
-		this.stringVal = v;
+	get string() { return this.#string; }
+	set string(v: string) { 
+		this.__string = v;
 	}
 
-	private refreshReactiveProperties(): void {
-		this._num = this.numVal;
-		this._string = this.stringVal;
-	}
-
+	// Initialize reactivity
 	#wireReactiveProperties = (() => {
 		Tree.on(this, "nodeChanged", () => {
-			this.refreshReactiveProperties();
+			this.#num = this.__num;
+			this.#string = this.__string;
 		});
 	})();
 }
