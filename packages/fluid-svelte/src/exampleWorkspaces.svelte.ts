8154ec443cb740b0f7520bc8526e1e9415e3d6bb
Tyler Butler <tyler@tylerbutler.com>
2025-05-04 11:59:53 -0700

    wip

--- packages/fluid-svelte/src/exampleWorkspaces.svelte.ts
+++ packages/fluid-svelte/src/exampleWorkspaces.svelte.ts
@@ -1,15 +1,15 @@
 import { ReactiveStateWorkspace } from "$lib/ReactiveStateWorkspace.svelte.js";
-import { StateFactory } from "@fluidframework/presence/alpha";
+import { StateFactory, StatesWorkspace } from "@fluidframework/presence/alpha";
+
+const appPresence = presence.states.getWorkspace("app:v1", {});
 
 interface MouseCoordinate {
 	x: number;
 	y: number;
 }
 
-const ws = StateFactory.latest<MouseCoordinate>({
-  local: { x: 0, y: 0 },
+const ws = appPresence. StateFactory.latest<MouseCoordinate>({
+	local: { x: 0, y: 0 },
 });
 
-const mouseTracker = new ReactiveStateWorkspace<MouseCoordinate>(
-	ws,
-);
+const mouseTracker = new ReactiveStateWorkspace<MouseCoordinate>(ws);
--- packages/fluid-svelte/src/lib/ReactiveStateWorkspace.svelte.ts
+++ packages/fluid-svelte/src/lib/ReactiveStateWorkspace.svelte.ts
@@ -5,18 +5,26 @@ import type {
 } from "@fluidframework/presence/alpha";
 import { SvelteMap } from "svelte/reactivity";
 
+// temporary type
+type Latest<T> = LatestRaw<T>;
+
 /**
  * A class that wraps a Fluid Framework presence state workspace in Svelte state so the data becomes reactive when used
  * in Svelte. This enables easy binding of Presence data in the view/template.
  */
 export class ReactiveStateWorkspace<
 	T extends object,
-	L extends LatestRaw<T> = LatestRaw<T>,
+	L extends LatestRaw<T> | Latest<T> = LatestRaw<T>,
 > {
+	/**
+	 * Storage for the reactive state. The map is keyed by Attendee.
+	 */
 	protected readonly reactiveState = $state(new SvelteMap<Attendee, T>());
 
-	public readonly unfilteredData = $derived(this.reactiveState);
-
+	/**
+	 * Visible reactive presence data. Disconnected attendees are excluded. Use `unfilteredData` if you want to include
+	 * disconnected attendees.
+	 */
 	public readonly data = $derived.by(() => {
 		return new SvelteMap(
 			[...this.reactiveState].filter(
@@ -24,6 +32,11 @@ export class ReactiveStateWorkspace<
 			),
 		);
 	});
+
+	/**
+	 * All reactive presence data, including that of disconnected attendees.
+	 */
+	public readonly unfilteredData = $derived(this.reactiveState);
 
 	public get local() {
 		return this.latest.local;
