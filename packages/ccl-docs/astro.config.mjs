aa800cd714a4277df184e1396962bec6063420c5
Tyler Butler <tyler@tylerbutler.com>
2025-10-02 21:25:02 -0700

    ccl grammar

--- packages/ccl-docs/astro.config.mjs
+++ packages/ccl-docs/astro.config.mjs
@@ -1,3 +1,6 @@
+import fs from "node:fs";
+import path from "node:path";
+
 import netlify from "@astrojs/netlify";
 import starlight from "@astrojs/starlight";
 import a11yEmoji from "@fec/remark-a11y-emoji";
@@ -11,6 +14,13 @@ const scriptUrl = new URL(import.meta.url);
 
 // Get the directory name from the script URL
 const rootDir = new URL("../..", import.meta.url).pathname;
+
+const cclGrammar = JSON.parse(
+	fs.readFileSync(
+		path.join(path.dirname(scriptUrl.pathname), "ccl-grammar.json"),
+		"utf-8",
+	),
+);
 
 // https://astro.build/config
 export default defineConfig({
@@ -26,20 +36,24 @@ export default defineConfig({
 			lastUpdated: true,
 			customCss: [
 				// Fontsource files for to regular and semi-bold font weights.
-				"@fontsource/ibm-plex-serif/400.css",
-				"@fontsource/ibm-plex-serif/600.css",
+				// "@fontsource/ibm-plex-serif/400.css",
+				// "@fontsource/ibm-plex-serif/600.css",
 				"@fontsource/metropolis/400.css",
 				"@fontsource/metropolis/600.css",
-				"@fontsource/ibm-plex-mono/400.css",
-				"@fontsource/ibm-plex-mono/600.css",
+				// "@fontsource/ibm-plex-mono/400.css",
+				// "@fontsource/ibm-plex-mono/600.css",
 				"./src/styles/custom.css",
 			],
 			plugins: [starlightLinksValidator(), starlightLLMsTxt()],
 			expressiveCode: {
 				shiki: {
 					langs: [
-						JSON.parse(fs.readFileSync("./ccl-grammar.json", "utf-8")),
+						cclGrammar,
+						// JSON.parse(fs.readFileSync(path.join(rootDir, "ccl-grammar.json"), "utf-8")),
 					],
+					langAlias: {
+						catcl: "ccl",
+					},
 				},
 				styleOverrides: {
 					codeFontFamily:
