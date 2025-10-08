bb351e6cf17dcceed1e0e358c11cacef39e3eaaa
Tyler Butler <tyler@tylerbutler.com>
2025-10-07 17:56:56 -0700

    disable base16 tailwind plugin

--- packages/ccl-test-viewer/tailwind.config.js
+++ packages/ccl-test-viewer/tailwind.config.js
@@ -6,7 +6,7 @@ const config = {
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 
 	// Base16 plugin - keep this in JS config since it's a complex plugin
-	plugins: [base16Tailwind()],
+	// plugins: [base16Tailwind()],
 };
 
 export default config;
